626bb957be5f0a88c4f62396403790ee2a4d7af7
smoot <smoothtacomann@gmail.com>
2018-12-24 15:42:52 -0500

    added a few lines of boilerplate

---
+++ server.js
@@ -0,0 +1,6 @@
+//Dependencies
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// Making a request via axios 
+axios.get("https://old.reddit.com/r/webdev/").then(function(response) {});
