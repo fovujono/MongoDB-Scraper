3a03a55cce036ac2e1a8041d45b4185aefac81ed
smoot <smoothtacomann@gmail.com>
2019-01-11 21:51:07 -0500

    styling

--- public/app.js
+++ public/app.js
@@ -2,8 +2,8 @@
 $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    // Display the information on the page
+    $("#articles").append("<p data-id='" + data[i]._id +  "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
 });
 
@@ -70,3 +70,4 @@ $(document).on("click", "#savenote", function() {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
